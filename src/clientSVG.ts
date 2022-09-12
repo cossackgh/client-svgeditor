1937b4588f81b22046384a9906963e35fcacc1fd
DMITRY YAGUPOV <cossack_box@mail.ru>
2022-09-12 17:35:34 +0300

    feat(pill): add zoom to mobile mode

--- src/clientSVG.ts
+++ src/clientSVG.ts
@@ -10,6 +10,7 @@ import type {
 
 import Base from './base'
 import { doc } from 'prettier'
+import { visitFunctionBody } from 'typescript'
 
 export class ClientSVGEditor extends Base {
   node: any
@@ -56,6 +57,9 @@ export class ClientSVGEditor extends Base {
   nodeBallon: Baloon | null
   customNodeBallon!: HTMLElement | null
   isMobile: boolean | undefined
+  evCache: any[] | undefined
+  prevDiff: number = -1
+  currentZoom: number = 1
 
   constructor(
     node: any,
@@ -176,7 +180,9 @@ export class ClientSVGEditor extends Base {
 
   start() {
     this.isMobile = isMobile()
+    this.evCache = []
     console.log('isMobile = ', this.isMobile)
+
     if (this.options.urlmap !== '') {
       this.insertSVG(this.options.urlmap!)
     }
@@ -190,6 +196,7 @@ export class ClientSVGEditor extends Base {
     }
 
     this.node.style.backgroundColor = this.options.mapTheme!.colorBG
+    this.node.style.overflow = 'auto'
     return true
   }
   async insertSVG(itemSVG: string) {
@@ -203,7 +210,10 @@ export class ClientSVGEditor extends Base {
     }
     this.initInteractiveLayer()
     this.clearInteractiveLayer()
-
+    if (this.isMobile) {
+      this.initZoom()
+      this.initMobileBalloon()
+    }
     //create object optionsBaloon type BaloonOptions
     const optionsBaloon: BaloonOptions = {
       baloonTheme: this.baloonTheme,
@@ -212,12 +222,16 @@ export class ClientSVGEditor extends Base {
 
     if (this.options.isCustomBalloon) {
       //this.customNodeBallon = this.customNodeBallon
-      this.nodeBallon = createCustomBaloon(
-        optionsBaloon,
-        this.options.nodeCustomBalloon!
-      )
+      if (!this.isMobile) {
+        this.nodeBallon = createCustomBaloon(
+          optionsBaloon,
+          this.options.nodeCustomBalloon!
+        )
+      }
     } else {
-      this.nodeBallon = createBaloon(optionsBaloon)
+      if (!this.isMobile) {
+        this.nodeBallon = createBaloon(optionsBaloon)
+      }
     }
 
     if (this.options.title !== '') {
@@ -376,8 +390,180 @@ export class ClientSVGEditor extends Base {
     //  console.log(`params =`, params)
     func(params)
   }
+
+  // get function for mobile pointerevent  for zoom effect
+
+  initZoom() {
+    const el = this.node
+    el!.onpointerdown = this.pointerdownHandler.bind(this)
+    el!.onpointermove = this.pointermoveHandler.bind(this)
+    console.log(this.isMobile)
+    // Use same handler for pointer{up,cancel,out,leave} events since
+    // the semantics for these events - in this app - are the same.
+    /*     el!.onpointerup = pointerupHandler;*/
+    el!.onpointercancel = this.pointerupHandler.bind(this)
+    el!.onpointerout = this.pointerupHandler.bind(this)
+    el!.onpointerleave = this.pointerupHandler.bind(this)
+  }
+
+  pointerdownHandler(ev: any) {
+    // The pointerdown event signals the start of a touch interaction.
+    // This event is cached to support 2-finger gestures
+    console.log('pointerDown', ev)
+    console.log('mob  == ', this.evCache)
+
+    this.evCache!.push(ev)
+  }
+
+  pointermoveHandler(ev: any) {
+    // This function implements a 2-pointer horizontal pinch/zoom gesture.
+    //
+    // If the distance between the two pointers has increased (zoom in),
+    // the target element's background is changed to "pink" and if the
+    // distance is decreasing (zoom out), the color is changed to "lightblue".
+    //
+    // This function sets the target element's border to "dashed" to visually
+    // indicate the pointer's target received a move event.
+    console.log('pointerMove', ev)
+    //ev.target.style.border = 'dashed'
+
+    // Find this event in the cache and update its record with this event
+    const index = this.evCache!.findIndex(
+      (cachedEv: any) => cachedEv.pointerId === ev.pointerId
+    )
+    this.evCache![index] = ev
+
+    // If two pointers are down, check for pinch gestures
+    if (this.evCache!.length === 2) {
+      // Calculate the distance between the two pointers
+      const curDiff = Math.abs(
+        this.evCache![0].clientX - this.evCache![1].clientX
+      )
+
+      if (this.prevDiff > 0) {
+        if (curDiff > this.prevDiff) {
+          // The distance between the two pointers has increased
+          // Увеличение
+          this.currentZoom = this.currentZoom + Math.floor(curDiff / 10) / 100
+          console.log('Pinch moving OUT -> Zoom in', Math.floor(curDiff / 10))
+        }
+        if (this.currentZoom > 5) {
+          this.currentZoom = 5
+        }
+        console.log('this.currentZoom = ', this.currentZoom)
+        //ev.target.style.background = 'pink'
+      }
+      if (curDiff < this.prevDiff) {
+        // The distance between the two pointers has decreased
+        //Уменьшение
+        console.log('Pinch moving IN -> Zoom out', this.currentZoom)
+        //ev.target.style.background = 'lightblue'
+        this.currentZoom = this.currentZoom - Math.floor(curDiff / 10) / 100
+        if (this.currentZoom < 0.5) {
+          this.currentZoom = 0.5
+        }
+      }
+      // Cache the distance for the next move event
+      this.prevDiff = curDiff
+      this.node!.firstChild.style.transform = `scale(${this.currentZoom})` //  scale(1.5)
+      this.node!.firstChild.style.transformOrigin = 'center'
+    }
+  }
+
+  pointerupHandler(ev: any) {
+    console.log(ev.type, ev)
+    // Remove this pointer from the cache and reset the target's
+    // background and border
+    //this.removeEvent(ev)
+
+    const index = this.evCache!.findIndex(
+      (cachedEv: any) => cachedEv.pointerId === ev.pointerId
+    )
+    this.evCache!.splice(index, 1)
+
+    //ev.target.style.background = "white";
+    //ev.target.style.border = "1px solid black";
+
+    // If the number of pointers down is less than two then reset diff tracker
+    if (this.evCache!.length < 2) {
+      //this.prevDiff = -1
+      //  this.currentZoom = Math.floor(this.prevDiff / 10) / 10
+      console.log('this.currentZoom = ', this.currentZoom)
+    }
+  }
+  removeEvent(ev: any) {
+    // Remove this event from the target's cache
+    const index = this.evCache!.findIndex(
+      (cachedEv: any) => cachedEv.pointerId === ev.pointerId
+    )
+    this.evCache!.splice(index, 1)
+  }
+
+  initMobileBalloon = () => {
+    const interactiveLayer = this.node.querySelector(
+      this.options.interactiveLayer
+    )
+    interactiveLayer.addEventListener('click', (e: any) => {
+      console.log('click', e.target.id)
+      e.stopPropagation()
+      /* if (e.target.tagName !== 'g') {
+      this.onClickMobile(
+        this.options.funcClick,
+        this.dataItems.find((item) => item.idmap === e.target.id)
+      )
+    } */
+    })
+
+    if (document.querySelector('#BaloonMobile') === null) {
+      const baloonMobile = document.createElement('div')
+      baloonMobile.style.position = 'fixed'
+      baloonMobile.style.bottom = '0px'
+      baloonMobile.style.left = '0px'
+      baloonMobile.style.height = '100px'
+      baloonMobile.style.width = '100%'
+
+      baloonMobile.id = 'BaloonMobile'
+      baloonMobile.style.display = 'none'
+      document.body.appendChild(baloonMobile)
+      baloonMobile.innerHTML = `
+    <div  style="background-color: ${this.baloonTheme.colorBG}; border-radius: 10px 10px 0 0; box-shadow: 0 0 10px rgba(0,0,0,0.5); width: 100%; height: 100%;">
+    <div id="balloonItemMobile">
+    </div>
+    </div>`
+
+      // add event listener click over #interactiveLayer
+      document.body.addEventListener('click', (e) => {
+        if (e.target !== baloonMobile) {
+          // console.log('click', e.target)
+          baloonMobile!.style.display = 'none'
+        }
+      })
+    }
+  }
+
   onClickMobile(func: any, params: any) {
     console.log(`onClickMobile params =`, params)
+    const balloonMobileRoot = document.getElementById('BaloonMobile')
+    balloonMobileRoot!.style.display = 'block'
+    const balloonItem = document.getElementById('balloonItemMobile')
+    console.log(`onClickMobile params =`, balloonItem)
+    balloonItem!.innerHTML = `
+    <div style="display:flex;flex-direction: column;">
+    <div style="padding:4px;    text-align: center; font-size:1.2em;font-weight:600;">
+    ${params.title}
+    </div>
+    <div style="padding:4px;display:flex;justify-content: space-between;align-items: center;">
+<div>
+    ${params.description}
+    </div>
+    <a href="${params.slug}" target="_self" style="padding:4px;">
+      <button  style="height: 40px;" >Посмотреть</button>
+    </a>
+    </div>
+    
+    </div>
+    `
+
     //func(params)
   }
   onHover(ev: any) {
@@ -394,45 +580,46 @@ export class ClientSVGEditor extends Base {
     }
 
     ev.style.opacity = this.options.mapTheme!.opacityHoverItem
-
-    if (this.options.isCustomBalloon) {
-      if (getDataItem !== undefined) {
-        if (getDataItem?.description !== undefined) {
-          this.nodeBallon!.customRender(
-            {
-              title: getDataItem?.title,
-              description: getDataItem?.description,
-            },
-            this.options.dataStructureCustomBalloon
-          )
-        } else {
-          this.nodeBallon!.customRender(
-            {
-              title: getDataItem?.title,
-              description: '',
-            },
-            this.options.dataStructureCustomBalloon
-          )
-        }
-      }
-    } else {
-      if (getDataItem !== undefined) {
-        if (getDataItem?.description !== undefined) {
-          this.nodeBallon!.render({
-            title: getDataItem?.title,
-            description: getDataItem?.description,
-          })
-        } else {
-          this.nodeBallon!.render({
-            title: getDataItem?.title,
-            description: '',
-          })
+    if (!this.isMobile) {
+      if (this.options.isCustomBalloon) {
+        if (getDataItem !== undefined) {
+          if (getDataItem?.description !== undefined) {
+            this.nodeBallon!.customRender(
+              {
+                title: getDataItem?.title,
+                description: getDataItem?.description,
+              },
+              this.options.dataStructureCustomBalloon
+            )
+          } else {
+            this.nodeBallon!.customRender(
+              {
+                title: getDataItem?.title,
+                description: '',
+              },
+              this.options.dataStructureCustomBalloon
+            )
+          }
         }
       } else {
-        this.nodeBallon!.render({
-          title: 'Info not found',
-          description: 'Info not found',
-        })
+        if (getDataItem !== undefined) {
+          if (getDataItem?.description !== undefined) {
+            this.nodeBallon!.render({
+              title: getDataItem?.title,
+              description: getDataItem?.description,
+            })
+          } else {
+            this.nodeBallon!.render({
+              title: getDataItem?.title,
+              description: '',
+            })
+          }
+        } else {
+          this.nodeBallon!.render({
+            title: 'Info not found',
+            description: 'Info not found',
+          })
+        }
       }
     }
   }
@@ -692,11 +879,22 @@ let handleMousemove = (
   }
 }
 
+// write function chek  mobile device
+
 const isMobile = () => {
-  return (
-    typeof window.orientation !== 'undefined' ||
-    navigator.userAgent.indexOf('IEMobile') !== -1
-  )
+  let check = false
+  ;((a) => {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true
+  })(navigator.userAgent || navigator.vendor)
+  return check
 }
 const throttle = (func: any, wait: number) => {
   //  console.log(`func =`, func)
