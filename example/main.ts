10b0d5af715cbd2c2396c6621d856b224770023c
DMITRY YAGUPOV <cossack_box@mail.ru>
2022-09-16 16:22:56 +0300

    feat(pill): fix example

--- example/main.ts
+++ example/main.ts
@@ -6,6 +6,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Example Interactive SVG map
     </p>
+    <p class="read-the-docs">
+      Based on <a href="https://github.com/cossackgh/client-svgeditor" target="_blank">client-svgeditor</a>
+    </p>
   </div>
 `
 
